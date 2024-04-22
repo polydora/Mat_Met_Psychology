cdaef4a69050670bbf0e916194ffb8d881fe5be8
Vadim Khaitov <polydora@rambler.ru>
2024-04-22 17:21:22 +0300

    Открыл задания для проектов

---
+++ Veronika_Sendvich.R
@@ -0,0 +1,19 @@
+# Бутерброды
+
+
+# В датасете SandwichAnts из пакета Stat2Data
+# содержатся данные о привлекательности сендвичей
+# с разной начинкой для муравьев (Mackisack, 1994;
+# Lock, 2013).
+
+# Если не получится загрузить данные из пакета,
+# скачайте файл SandwichAnts по ссылке
+# https://www.rdocumentation.org/packages/Stat2Data/versions/2.0.0/topics/SandwichAnts
+
+# Задача - изучить то, на бутерброд с какой начинкой и с маслом или без (плюс взаимодействие факторов), заползает больше муравьев
+
+library(Stat2Data)
+data(SandwichAnts)
+?SandwichAnts
+
+
